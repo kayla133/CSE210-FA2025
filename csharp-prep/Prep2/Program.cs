a5b8650a29348623cbbfab4d043a4118e5dc19e9
kayla133 <makaylahansen04@gmail.com>
2025-09-17 10:53:12 -0600

    finished the coding for this application and did the stretch activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+
+        string stretch = "";
+
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is; {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the course.");
+        }
+
+
+        if (percentage >= 90)
+        {
+            Console.WriteLine($"Your final grade is: {grade}");
+        }
+        else if (percentage < 67)
+        {
+            Console.WriteLine($"Your final grade is: {grade}");
+        }
+        else
+        {
+            int final = percentage / 10;
+
+            if (final >= 7)
+            {
+                stretch = "+";
+            }
+            else if (final < 3)
+            {
+                stretch = "-";
+            }
+
+            Console.WriteLine($"Your final grade is: {grade}{stretch}");
+        }
     }
 }
