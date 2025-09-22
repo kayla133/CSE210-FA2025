15633016406f223e5a8d5a44de3952746bbf773e
kayla133 <makaylahansen04@gmail.com>
2025-09-22 08:42:50 -0600

    created a notes page

---
+++ sandbox/Experimenting/Notes/Program.cs
@@ -0,0 +1,16 @@
+﻿//This is to keep track of notes in VScode.
+
+
+//How to Commit to GitHub through the terminal
+//git status
+//git add (each of the folders that showed up after the status command)
+//git status (double check everything is added)
+//git commit -m "(put message in quotes)"
+//git push
+
+//How to Add a new folder
+//change to proper directory using cd and cd ..
+//dotnet new console -o NAME
+//change to that directory
+//dotnet run (This is to run the program to make sure that it works and establish itself in the terminal)
+//Make sure to commit changes to GitHub
